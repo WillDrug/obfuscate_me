b78eeb7171e94eefc968f204a7a61a2034efcf47
willdrug <whydafuqdoineedthis@gmail.com>
2018-11-17 16:55:59 +0300

    Reworked a bit

--- win_clipboard_musher.py
+++ win_clipboard_musher.py
@@ -1,63 +1,81 @@
-from random import random
+import random
 import win32clipboard
 import re
+import pymorphy2
 # this should be switched up to separate files and such
 
-TRANSLATE = {
-    'ф': chr(1060), 'Ф': chr(1092),
-    'ё': 'ø', 'Ё': 'Ø',
-    'З': chr(1047), 'з': chr(1079),
-    'е': 'ѣ', 'Е': 'Ѣ',
-    'Ф': 'Ѳ', 'ф': 'ѳ',
-    # 'о': 'ѡ', 'О': 'Ѡ',
-    'з': chr(1109), 'З': chr(1029),
-    'Э': chr(1028), 'э': chr(1108),
-    'Ы': 'Ѵ', 'ы': 'ѵ',
-}
+THRESHOLD = 0.7
 
-WORDS = {
-    'писал': 'ѱалѣ', 'писать': 'ѱатѣ', 'пишу': 'ѱишу'
+
+TRANSLATE = {  # not using utf-8 codes to know which chars already used
+    'гь': ['Ꙉ'],
+    'жь': ['ʒ'],
+    'пь': ['ѱ'],
+    'пи': ['ѱ'],
+    'оу': ['ꙋ'],
+    'Ф': ['ф', 'ѳ'],
+    'я': ['ѧ'],
+    'ё': ['ø'],
+    'з': ['з', 'ѕ', 'ꙁ'],  # ß somehow does not pass upper() well =\ don't want to rewrite everything because of it
+    'и': ['ї'],
+    'е': ['ѣ'],
+    'ф': ['ѳ'],
+    'о': ['ѡ'],
+    'э': ['є'],
+    'у': ['ѫ'],
+    'г': ['ȝ', 'ћ'],
+    'д': ['ⰼ'],
 }
 
 VOWELS = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я']
 CONSONANTS = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ']
-
 BEFORE_HARD_MARK = ['б', 'в', 'г', 'д', 'ж', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш']
 
+dicts = pymorphy2.analyzer.MorphAnalyzer()
 
-def replace_keep_case(word, replacement, text):
-    def func(match):
-        g = match.group()
-        if g.islower(): return replacement.lower()
-        if g.istitle(): return replacement.title()
-        if g.isupper(): return replacement.upper()
-        return replacement
-    return re.sub(word, func, text, flags=re.I)
+
+def normalize(match):
+    g = match.group()
+    # normalize by morpher
+    replacement = dicts.parse(g)[0].word
+    if replacement.__len__() == g.__len__():
+        repl = [q for q in replacement]
+        matl = [q for q in g]
+        if repl.__len__() == matl.__len__():
+            for i in range(matl.__len__()):
+                if matl[i].islower():
+                    repl[i] = repl[i].lower()
+                else:
+                    repl[i] = repl[i].upper()
+            replacement = ''.join(repl)
+
+    # now how about some hard marks... not everywhere, of course.
+    if replacement[-1] in BEFORE_HARD_MARK and random.random() > THRESHOLD:
+        replacement = replacement + ('Ҍ' if replacement[-1].isupper() else 'ҍ')
+    # now let's choose one substitute for each letter available to change up
+    letters = {q: TRANSLATE[q][random.randint(0, TRANSLATE[q].__len__()-1)] for q in TRANSLATE}
+    for letter in letters:
+        if random.random() > THRESHOLD:
+            for i in re.finditer(letter, replacement.lower()):
+                found = replacement[i.span()[0]:i.span()[1]]
+                if found.isupper():
+                    replacement = replacement[0:i.span()[0]] + letters[letter].upper() + replacement[i.span()[1]:]
+                elif found.islower():
+                    replacement = replacement[0:i.span()[0]] + letters[letter].lower() + replacement[i.span()[1]:]
+                elif found.istitle():
+                    replacement = replacement[0:i.span()[0]] + letters[letter].title() + replacement[i.span()[1]:]
+                else:
+                    replacement = replacement[0:i.span()[0]] + letters[letter] + replacement[i.span()[1]:]
+    return replacement
+
 
 def mush(data):
-    for word in WORDS:
-        # test = re.compile(f"(?i)(?=((?:\W|\n|^){word}(?:\W|\n|$)))", flags=re.MULTILINE)
-        data = replace_keep_case(word, WORDS[word], data)
-    new_data = list()
-    for i in range(data.__len__()):
-        new_data.append(data[i])
-        if (i == data.__len__()-1 or data[i+1] in [' ', ',', '.', ';', '!', '?']) and data[i] in BEFORE_HARD_MARK and (random()>0.55):
-            new_data.append('Ѣ'.lower())
-    # add here differentiation between "мир" as in world and as in peace and change up accordingly.
-    data = ''.join(new_data)
-    # proper i-s and other word-by-word change
-    new_data = list()
-    for word in data.split(' '):
-        for i in range(word.__len__()-1):
-            if word[i] in ['и', 'И'] and i < word.__len__() - 1:
-                if word[i + 1].lower() in ['й', ] + VOWELS:
-                    word = word[0:i] + (chr(1031) if word[i].isupper() else chr(1111)) + word[i + 1:]
-        new_data.append(word)
-    data = ' '.join(new_data)
-    for char in TRANSLATE:
-        data = data.replace(char, TRANSLATE[char])
-    # Мироздание требует возмездия за такие выкрутасы
+    test = re.compile("\w+")  #each word
+    data = re.sub(test, normalize, data)
+    # print(data)
     return data
+
+# Съешь еще этих, СЪЕШЬ! мягких)) ЕщЕ *французских* :( булок да... да выпей чаю, зелибоба? Гьрь
 
 win32clipboard.OpenClipboard()
 cdata = win32clipboard.GetClipboardData()#.encode('utf-8')
